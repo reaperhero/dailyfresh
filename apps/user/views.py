af25d9b092ea4c7024eeb79366a0b14d17196803
chenqiangjun <chenqiangjun@mudu.tv>
2020-07-12 12:56:20 +0800

    用户注册逻辑

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import re
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from user.models import User
+
 
 # Create your views here.
+
+
+# /user/register
+
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def register_handle(request):
+    # 注册处理
+    username = request.POST.get('user_name')
+    password = request.POST.get('pwd')
+    email = request.POST.get('email')
+    allow = request.POST.get('allow')
+    # 数据是否为空
+    if not all([username, password, email]):
+        print("as")
+        return render(request, 'register.html', {
+            'errmsg': '数据不完整'
+        })
+
+    # 校验邮箱
+    if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+        print("ad")
+        return render(request, 'register.html', {
+            'errmsg': '邮箱格式不正确'
+        })
+
+    if allow != 'on':
+        print("as")
+        return render(request, 'register.html', {
+            'errmsg': '请同意协议'
+        })
+
+    try:
+        user = User.objects.get(username=username)
+    except user.DoesNotExist:
+        # 用户已存在
+        user = None
+    if user:
+        return render(request, 'register.html', {
+            'errmsg': '用户名已存在'
+        })
+    user = User.objects.create_user(username, email, password)
+    user.is_active = 0
+    user.save()
+    return redirect(reverse('goods:index'))
--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tinymce', # 富文本
+    'tinymce',  # 富文本
     'user',  # 用户模块
     'goods',  # 商品模块
     'cart',  # 购物车模块
@@ -77,7 +76,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dailyfresh.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -95,7 +93,7 @@ DATABASES = {
 # Django认证系统使用的模型类
 # 替换django默认账户
 # python manage.py createsuper
-AUTH_USER_MODEL='user.User'
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -109,7 +107,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
