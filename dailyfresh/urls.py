c8411ddaa35f55b9a4380679b2ae2098a7675ee5
chenqiangjun <chenqiangjun@mudu.tv>
2020-07-12 11:06:22 +0800

    项目框架搭建

---
+++ dailyfresh/urls.py
@@ -0,0 +1,28 @@
+"""dailyfresh URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path, include
+from django.contrib import admin
+from django.conf.urls import url
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^tinymce/', include('tinymce.urls')),  # 富文本编辑器
+    url(r'^user/', include('user.urls'), name='user'),  # 用户模块
+    url(r'^cart/', include('cart.urls'), name='cart'),  # 购物车模块
+    url(r'^order/', include('order.urls'), name='order'),  # 订单模块
+    url(r'^', include('goods.urls'), name='goods'),  # 商品模块
+]
